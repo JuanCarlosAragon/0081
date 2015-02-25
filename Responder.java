5ebb32d24d6ceb0f80534bfddf2ac61dbb1e875d
Juan Carlos Aragón <juankar.md@gmail.com>
2015-02-25 12:32:55 +0100

    Modificada la clase Responder para que conteste con una respuesta al azar entre un arrayList de strings

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,19 @@
  */
 public class Responder
 {
+    private Random rnd;
+    private ArrayList<String> contestaciones;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        rnd = new Random();
+        contestaciones = new ArrayList<>();
+        contestaciones.add("Claro, claro... te escucho");
+        contestaciones.add("Sigue hablando, me interesa mucho");
+        contestaciones.add("Aham... entiendo...");
+        contestaciones.add("si?? hablas en serio??");
     }
 
     /**
@@ -20,6 +30,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        String contestacion = contestaciones.get(rnd.nextInt(contestaciones.size()));
+        return contestacion;
     }
 }
